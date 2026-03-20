bfafbbb407c785ecd8fc083eac31397475599f1e
Nicobriss78 <nicobriss@yahoo.com>
2026-03-20 06:45:58 +0100

    Update home-controller.js

--- js/home/home-controller.js
+++ js/home/home-controller.js
@@ -14,6 +14,7 @@ import {
   renderRail,
 } from "./home-renderer.js";
 
+import { apiGet, getActiveBannersBatch } from "../api.js";
 import { createBannerEngine } from "./home-banners.js";
 /* =========================================================
    CONFIG
