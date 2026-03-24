2af928f76c7b994153e00806886c840f90e353ce
Nicobriss78 <nicobriss@yahoo.com>
2026-03-24 15:18:07 +0100

    Update seguiti-state.js

--- js/seguiti/seguiti-state.js
+++ js/seguiti/seguiti-state.js
@@ -1,1 +1,15 @@
+export const seguitiState = {
+  rawEvents: [],
+  normalizedEvents: [],
+  organizerSections: [],
+  loading: false,
+  error: null,
+  initialized: false,
+  activeSectionIndex: 0,
+  bannerEngine: null,
+};
 
+export const seguitiSession = {
+  currentUserId: null,
+  currentUserProfile: null,
+};
