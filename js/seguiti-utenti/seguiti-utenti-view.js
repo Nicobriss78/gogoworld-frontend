97a3e729818eacfa0be7d1b9a9e90d8c586e30e7
Nicobriss78 <nicobriss@yahoo.com>
2026-03-27 17:18:43 +0100

    Update seguiti-utenti-view.js

--- js/seguiti-utenti/seguiti-utenti-view.js
+++ js/seguiti-utenti/seguiti-utenti-view.js
@@ -1,1 +1,47 @@
+const REQUIRED_SELECTORS = {
+  greeting: "#seguitiUtentiGreeting",
+  roleLabel: "#seguitiUtentiRoleLabel",
+  notificationsBtn: "#seguitiUtentiNotificationsBtn",
+  menuBtn: "#seguitiUtentiMenuBtn",
+  root: "#seguitiUtentiRoot",
+  messageArea: "#seguitiUtentiMessageArea",
+  loadingState: "#seguitiUtentiLoadingState",
+  errorState: "#seguitiUtentiErrorState",
+  errorText: "#seguitiUtentiErrorText",
+  retryBtn: "#seguitiUtentiRetryBtn",
+  emptyState: "#seguitiUtentiEmptyState",
+  listWrap: "#seguitiUtentiListWrap",
+  list: "#seguitiUtentiList",
+  cardTemplate: "#seguitiUtentiCardTemplate",
+};
 
+function queryRequiredElement(selector, label) {
+  const element = document.querySelector(selector);
+
+  if (!element) {
+    throw new Error(`[seguiti-utenti-view] Elemento obbligatorio mancante: ${label} (${selector})`);
+  }
+
+  return element;
+}
+
+export function getSeguitiUtentiView() {
+  const view = {
+    greeting: queryRequiredElement(REQUIRED_SELECTORS.greeting, "greeting"),
+    roleLabel: queryRequiredElement(REQUIRED_SELECTORS.roleLabel, "roleLabel"),
+    notificationsBtn: queryRequiredElement(REQUIRED_SELECTORS.notificationsBtn, "notificationsBtn"),
+    menuBtn: queryRequiredElement(REQUIRED_SELECTORS.menuBtn, "menuBtn"),
+    root: queryRequiredElement(REQUIRED_SELECTORS.root, "root"),
+    messageArea: queryRequiredElement(REQUIRED_SELECTORS.messageArea, "messageArea"),
+    loadingState: queryRequiredElement(REQUIRED_SELECTORS.loadingState, "loadingState"),
+    errorState: queryRequiredElement(REQUIRED_SELECTORS.errorState, "errorState"),
+    errorText: queryRequiredElement(REQUIRED_SELECTORS.errorText, "errorText"),
+    retryBtn: queryRequiredElement(REQUIRED_SELECTORS.retryBtn, "retryBtn"),
+    emptyState: queryRequiredElement(REQUIRED_SELECTORS.emptyState, "emptyState"),
+    listWrap: queryRequiredElement(REQUIRED_SELECTORS.listWrap, "listWrap"),
+    list: queryRequiredElement(REQUIRED_SELECTORS.list, "list"),
+    cardTemplate: queryRequiredElement(REQUIRED_SELECTORS.cardTemplate, "cardTemplate"),
+  };
+
+  return Object.freeze(view);
+}
