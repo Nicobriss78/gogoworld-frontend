699a41a40c82ff8b8bb27663bf554b5e91584cfc
Nicobriss78 <nicobriss@yahoo.com>
2026-03-28 09:53:45 +0100

    Update mappa-privati-controller.js

--- js/mappa-privati/mappa-privati-controller.js
+++ js/mappa-privati/mappa-privati-controller.js
@@ -87,6 +87,17 @@ const state = createMappaPrivatiState();
   function unbindUi() {
     elements.infoBtn?.removeEventListener("click", handleOpenDetail);
     elements.drawerContent?.removeEventListener("click", handleDrawerActions);
+
+    elements.menuBtn?.removeEventListener("click", handleToggleMenu);
+    elements.menuOverlay?.removeEventListener("click", handleCloseMenu);
+    elements.menuPanel?.removeEventListener("click", handleMenuPanelClick);
+
+    elements.unlockBtn?.removeEventListener("click", handleMenuUnlockPrivate);
+    elements.guideBtn?.removeEventListener("click", handleMenuGuide);
+    elements.switchRoleBtn?.removeEventListener("click", handleMenuSwitchRole);
+    elements.logoutBtn?.removeEventListener("click", handleMenuLogout);
+
+    document.removeEventListener("keydown", handleMenuEscape);
   }
 
   function handleDrawerActions(event) {
