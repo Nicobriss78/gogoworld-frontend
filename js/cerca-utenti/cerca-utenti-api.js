256f0030fb9c4809a2a1b2c99789c2fc46c050ab
Nicobriss78 <nicobriss@yahoo.com>
2026-04-08 20:17:24 +0200

    Update cerca-utenti-api.js

--- js/cerca-utenti/cerca-utenti-api.js
+++ js/cerca-utenti/cerca-utenti-api.js
@@ -1,1 +1,13 @@
+import { searchUsers, blockUser, unblockUser } from "../api.js";
 
+export async function searchUsersForView(query) {
+  return await searchUsers(query);
+}
+
+export async function blockUserForView(userId) {
+  return await blockUser(userId);
+}
+
+export async function unblockUserForView(userId) {
+  return await unblockUser(userId);
+}
