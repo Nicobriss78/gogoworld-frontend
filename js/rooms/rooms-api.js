054d6679e2d9b1e1632785e0de1fbda1a3c551da
Nicobriss78 <nicobriss@yahoo.com>
2026-04-12 10:53:12 +0200

    Update rooms-api.js

--- js/rooms/rooms-api.js
+++ js/rooms/rooms-api.js
@@ -1,1 +1,19 @@
+import {
+  openOrJoinEvent,
+  getEventRoomMeta,
+  listRoomMessages,
+  postRoomMessage,
+  markRoomRead,
+  getMyRooms,
+  getUnreadSummary,
+} from "../api.js";
 
+export {
+  openOrJoinEvent,
+  getEventRoomMeta,
+  listRoomMessages,
+  postRoomMessage,
+  markRoomRead,
+  getMyRooms,
+  getUnreadSummary,
+};
