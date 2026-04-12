9e55c2c90a1a660ea14c9b0ec7401bb89d785f87
Nicobriss78 <nicobriss@yahoo.com>
2026-04-12 19:11:04 +0200

    Update rooms-state.js

--- js/rooms/rooms-state.js
+++ js/rooms/rooms-state.js
@@ -3,20 +3,14 @@ export function createRoomsState() {
     roomId: null,
     eventId: null,
     roomMeta: null,
-    rooms: [],
     messages: [],
-    unreadSummary: [],
     returnTo: "/pages/home-v2.html",
 
-    // Stati di caricamento
-    isRoomsLoading: false,
-    isMessagesLoading: false,
-    isRoomMetaLoading: false,
+    isLoading: false,
     isSending: false,
-
-    // Stati UI
-    error: null,
-    empty: false,
     composerDisabled: false,
+
+    error: "",
+    empty: false,
   };
 }
