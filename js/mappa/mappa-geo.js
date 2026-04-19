109cb053175092f68dd19091aa30ecbebdc1dfc0
Nicobriss78 <nicobriss@yahoo.com>
2026-04-19 19:23:02 +0200

    Update mappa-geo.js

--- js/mappa/mappa-geo.js
+++ js/mappa/mappa-geo.js
@@ -42,9 +42,9 @@ export async function requestUserPosition() {
         });
       },
       {
-        enableHighAccuracy: false,
-        timeout: 15000,
-        maximumAge: 60000
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
       }
     );
   });
