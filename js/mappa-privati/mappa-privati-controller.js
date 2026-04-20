6a89345f8bb570740eb7f153ce944c8183b3f508
Nicobriss78 <nicobriss@yahoo.com>
2026-04-20 08:05:33 +0200

    Update mappa-privati-controller.js

--- js/mappa-privati/mappa-privati-controller.js
+++ js/mappa-privati/mappa-privati-controller.js
@@ -850,6 +850,9 @@ function getDomElements() {
     chatSend: document.getElementById("mappaChatSend"),
 
     infoBtn: document.getElementById("mappaChatInfoBtn"),
+    locateBtn: document.getElementById("mappaLocateBtn"),
+    locateBtnLabel: document.getElementById("mappaLocateBtnLabel"),
+    geoStatus: document.getElementById("mappaGeoStatus")
 locateBtn: document.getElementById("mappaLocateBtn"),
 geoStatus: document.getElementById("mappaGeoStatus")
   };
