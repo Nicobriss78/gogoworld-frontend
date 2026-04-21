a39c9de68d942593052ce1df9fc1de49978b93db
Nicobriss78 <nicobriss@yahoo.com>
2026-04-21 07:28:39 +0200

    Update mappa-privati-controller.js

--- js/mappa-privati/mappa-privati-controller.js
+++ js/mappa-privati/mappa-privati-controller.js
@@ -641,12 +641,16 @@ bindUi();
 
     state.setSelectedEvent(event);
     map.focusEvent(event.id);
+    scheduleMapRefresh();
+
     await chat.openForEvent(event);
+    scheduleMapRefresh();
 
     if (stored.returnDrawerOpen) {
       renderDetailCard(event);
       drawer.open();
       state.setDrawerOpen(true);
+      scheduleMapRefresh();
     }
 
     clearReturnContextStorage();
