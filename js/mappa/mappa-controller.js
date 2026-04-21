cf5ce277fb0f360c1801b00735402b8b26054a64
Nicobriss78 <nicobriss@yahoo.com>
2026-04-21 07:15:02 +0200

    Update mappa-controller.js

--- js/mappa/mappa-controller.js
+++ js/mappa/mappa-controller.js
@@ -623,12 +623,7 @@ document.addEventListener("visibilitychange", () => {
     ensureGeoWatchStarted();
   }
 
-  window.requestAnimationFrame(() => {
-    map.refreshLayout();
-    window.setTimeout(() => {
-      map.refreshLayout();
-    }, 120);
-  });
+  scheduleMapRefresh();
 });
   window.addEventListener("pagehide", (event) => {
     if (event.persisted) return;
