9565b6c22afd48e47421e9bbeedbc57abc3f0bd2
Nicobriss78 <nicobriss@yahoo.com>
2026-04-21 07:13:29 +0200

    Update mappa-controller.js

--- js/mappa/mappa-controller.js
+++ js/mappa/mappa-controller.js
@@ -554,12 +554,16 @@ function handleOpenFullChat(eventId) {
 
     state.setSelectedEvent(event);
     map.focusEvent(event.id);
+    scheduleMapRefresh();
+
     await chat.openForEvent(event);
+    scheduleMapRefresh();
 
     if (stored.returnDrawerOpen) {
       renderDetailCard(event);
       drawer.open();
       state.setDrawerOpen(true);
+      scheduleMapRefresh();
     }
 
     clearReturnContextStorage();
