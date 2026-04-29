3a87824603d465b2432460582b4cc663bbdc2489
Nicobriss78 <nicobriss@yahoo.com>
2026-04-29 14:52:28 +0200

    Update mappa-controller.js

--- js/mappa/mappa-controller.js
+++ js/mappa/mappa-controller.js
@@ -642,12 +642,9 @@ document.addEventListener("visibilitychange", () => {
     return;
   }
 
-  if (
-    (state.getState().geo?.mode || "explore") === "near_me" ||
-    (state.getState().geo?.mode || "explore") === GEO_FOLLOW_MODE
-  ) {
-    ensureGeoWatchStarted();
-  }
+  if ((state.getState().geo?.mode || "explore") === GEO_FOLLOW_MODE) {
+  ensureGeoWatchStarted();
+}
 
   scheduleMapRefresh();
 });
