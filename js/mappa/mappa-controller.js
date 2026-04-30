f856ca1730487805d6538912bd043cc977e1b9d8
Nicobriss78 <nicobriss@yahoo.com>
2026-04-30 08:17:18 +0200

    Update mappa-controller.js

--- js/mappa/mappa-controller.js
+++ js/mappa/mappa-controller.js
@@ -367,11 +367,7 @@ function calculateBearing(from, to) {
         fitBounds: false
       });
 
-      map.fitUserAndEvents(normalized, {
-        padding: [40, 40],
-        maxZoom: 15,
-        zoom: 15
-      });
+      map.setViewCenter(normalized, 15);
 
       setGeoStatus("Posizione rilevata. Ti mostro la tua area e gli eventi vicini.", "success");
     } catch (error) {
