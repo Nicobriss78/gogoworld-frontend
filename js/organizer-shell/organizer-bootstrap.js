a438eb32abcfa1f405cd8588a4d8507de6d8d0eb
Nicobriss78 <nicobriss@yahoo.com>
2026-05-03 21:41:55 +0200

    Update organizer-bootstrap.js

--- js/organizer-shell/organizer-bootstrap.js
+++ js/organizer-shell/organizer-bootstrap.js
@@ -1,11 +1,13 @@
 import { renderTopbar } from "./organizer-topbar.js";
 import { renderBottomnav } from "./organizer-bottomnav.js";
 import { checkAccess } from "./organizer-access-guard.js";
-import { loadIdentity } from "./organizer-identity.js";
 
 async function bootstrap() {
-  await checkAccess();
-  await loadIdentity();
+  const accessResult = await checkAccess();
+
+  if (!accessResult.allowed) {
+    return;
+  }
 
   renderTopbar();
   renderBottomnav();
