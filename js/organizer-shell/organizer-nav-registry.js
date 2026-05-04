d7785d3a2e6f2c97c6cac4952a39574368c5ab1d
Nicobriss78 <nicobriss@yahoo.com>
2026-05-04 04:44:02 +0200

    Update organizer-nav-registry.js

--- js/organizer-shell/organizer-nav-registry.js
+++ js/organizer-shell/organizer-nav-registry.js
@@ -1,3 +1,32 @@
 export const organizerNav = [
-  "dashboard",
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    href: "/pages/organizer-dashboard-v2.html",
+    enabled: true,
+  },
+  {
+    id: "events",
+    label: "Eventi",
+    href: "/pages/organizer-events-v2.html",
+    enabled: true,
+  },
+  {
+    id: "map",
+    label: "Mappa",
+    href: "#",
+    enabled: false,
+  },
+  {
+    id: "trills",
+    label: "Trilli",
+    href: "#",
+    enabled: false,
+  },
+  {
+    id: "promos",
+    label: "Promo",
+    href: "#",
+    enabled: false,
+  },
 ];
