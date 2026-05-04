ccb0d09ac30250ede9eae8ebac2f8a57aef0aca2
Nicobriss78 <nicobriss@yahoo.com>
2026-05-04 04:38:36 +0200

    Create organizer-events-state.js

---
+++ js/organizer-events/organizer-events-state.js
@@ -0,0 +1,11 @@
+export const eventsState = {
+  loading: false,
+  error: null,
+  events: [],
+  filters: {
+    query: "",
+    approvalStatus: "all",
+    visibility: "all",
+    privacy: "all",
+  },
+};
