14bcf161a63a2927b3f664e18adde45d8c2ff5f1
Nicobriss78 <nicobriss@yahoo.com>
2026-05-06 21:50:08 +0200

    Update organizer-event-detail-state.js

--- js/organizer-event-detail/organizer-event-detail-state.js
+++ js/organizer-event-detail/organizer-event-detail-state.js
@@ -1,5 +1,10 @@
 export const organizerEventDetailState = {
   loading: true,
   error: null,
+  actionError: null,
+  actionMessage: null,
   event: null,
+  confirmDelete: false,
+  deleting: false,
+  openingRoom: false,
 };
