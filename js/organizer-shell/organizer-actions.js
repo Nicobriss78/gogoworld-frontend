3d7af546332aaabe4a588c7340561a1336ef63d1
Nicobriss78 <nicobriss@yahoo.com>
2026-05-09 18:31:21 +0200

    Update organizer-actions.js

--- js/organizer-shell/organizer-actions.js
+++ js/organizer-shell/organizer-actions.js
@@ -1,3 +1,10 @@
 export function logout() {
-  console.log("logout");
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  localStorage.removeItem("currentUser");
+  window.location.href = "/index.html";
+}
+
+export function openNotifications() {
+  window.location.href = "/pages/notifiche-v2.html?rootReturnTo=organizer";
 }
