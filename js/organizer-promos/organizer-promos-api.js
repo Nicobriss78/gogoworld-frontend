d9da6a4e25bf51f4b2c467ad826f92d8637fb9f1
Nicobriss78 <nicobriss@yahoo.com>
2026-05-15 13:52:38 +0200

    Create organizer-promos-api.js

---
+++ js/organizer-promos/organizer-promos-api.js
@@ -0,0 +1,21 @@
+// js/organizer-promos/organizer-promos-api.js
+// API Promozioni Organizer V2
+
+import { apiGet } from "../api.js";
+
+export async function fetchOrganizerPromos(params = {}) {
+  const query = new URLSearchParams();
+
+  if (params.status && params.status !== "ALL") {
+    query.set("status", params.status);
+  }
+
+  if (params.placement && params.placement !== "ALL") {
+    query.set("placement", params.placement);
+  }
+
+  const qs = query.toString();
+  const endpoint = qs ? `/banners/mine?${qs}` : "/banners/mine";
+
+  return apiGet(endpoint);
+}
