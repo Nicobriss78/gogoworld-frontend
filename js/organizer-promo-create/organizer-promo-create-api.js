4a95710d605fa45b97385092a12ed98db700c103
Nicobriss78 <nicobriss@yahoo.com>
2026-05-15 18:13:08 +0200

    Create organizer-promo-create-api.js

---
+++ js/organizer-promo-create/organizer-promo-create-api.js
@@ -0,0 +1,30 @@
+// js/organizer-promo-create/organizer-promo-create-api.js
+// API Organizer Promo Create V2
+
+import { apiGet, apiPost } from "../api.js";
+
+export async function fetchOrganizerEvents() {
+  const response = await apiGet("/events/organizer");
+
+  if (Array.isArray(response)) {
+    return response;
+  }
+
+  if (Array.isArray(response?.data)) {
+    return response.data;
+  }
+
+  if (Array.isArray(response?.events)) {
+    return response.events;
+  }
+
+  return [];
+}
+
+export async function estimatePromo(payload) {
+  return apiPost("/banners/estimate", payload);
+}
+
+export async function submitPromo(payload) {
+  return apiPost("/banners/submit", payload);
+}
