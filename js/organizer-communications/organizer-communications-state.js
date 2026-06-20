1c3ddfc00303e09008e8d7262a55545e82428778
Nicobriss78 <nicobriss@yahoo.com>
2026-06-20 08:48:59 +0200

    Create organizer-communications-state.js

---
+++ js/organizer-communications/organizer-communications-state.js
@@ -0,0 +1,12 @@
+export const communicationsState = {
+  loading: false,
+  error: "",
+  summary: {
+    notificationsUnread: 0,
+    roomsUnread: 0,
+    dmUnread: 0,
+    totalUnread: 0,
+    recentNotifications: [],
+    recentRooms: [],
+  },
+};
