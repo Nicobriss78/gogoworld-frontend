bbc2a015e68a2ffe38645645f9eb0e0b77f2b878
Nicobriss78 <nicobriss@yahoo.com>
2026-06-25 22:05:11 +0200

    Import syncLocationIfAlreadyGranted function

--- js/shared-shell/shared-bootstrap.js
+++ js/shared-shell/shared-bootstrap.js
@@ -13,6 +13,7 @@ import { mountTopbar } from "./shared-topbar.js";
 import { mountMenu, setMenuOpen } from "./shared-menu.js";
 import { mountBottomnav } from "./shared-bottomnav.js";
 import { initNotificationsCenter } from "../shared/notifications-center.js";
+import { syncLocationIfAlreadyGranted } from "../shared/shared-geo-consent.js";
 let notificationsCenter = null;
 const SHARED_ICON_SPRITE_ID = "shared-v2-icon-sprite";
 
