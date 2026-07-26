5fba7d2ca5c90cdfbd7890ac80fdcf9f5aa09af1
Nicobriss78 <nicobriss@yahoo.com>
2026-07-26 11:30:19 +0200

    Update shared-bootstrap.js

--- js/shared-shell/shared-bootstrap.js
+++ js/shared-shell/shared-bootstrap.js
@@ -14,9 +14,6 @@ import { mountMenu, setMenuOpen } from "./shared-menu.js";
 import { mountBottomnav } from "./shared-bottomnav.js";
 import { initNotificationsCenter } from "../shared/notifications-center.js";
 import { mountSharedGeoBanner } from "../shared/shared-geo-banner.js";
-import {
-  resumeParticipantGeoTrackingIfEnabled,
-} from "../shared/participant-geo-tracking-session.js";
 
 let notificationsCenter = null;
 const SHARED_ICON_SPRITE_ID = "shared-v2-icon-sprite";
